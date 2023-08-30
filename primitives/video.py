3f391dcaad7f3c6ccbbead9fc986567f560eb4af
fumiko <2394425147@qq.com>
2023-08-30 20:29:58 +0800

    feat: Implemented all supported primitive types

---
+++ primitives/video.py
@@ -0,0 +1,17 @@
+﻿from core import timing
+from core.storyboard import storyboard_definition
+from primitives.base import PrimitiveBase
+
+
+class Video(PrimitiveBase):
+    def __init__(self):
+        super().__init__()
+
+    pass
+
+
+def create() -> Video:
+    video = Video()
+    storyboard_definition.videos.append(video)
+    video.time = timing.current_time
+    return video
